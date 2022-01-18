261886f4e895e490947223344a15436e0bb4702c
Ruslan Sattarov <r8sattarov@mail.ru>
2022-01-18 21:02:29 +0500

    init: start task portfolio part two, feat: implement menu burger, also adaptive to width 320px.

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,4 +1,23 @@
 'use strict';
+
+//-------menu-burger--------------------//
+
+const burgerIcon = document.querySelector('.header__icon');
+const menuNav = document.querySelector('.header__nav');
+if (burgerIcon) {
+   burgerIcon.addEventListener('click', function (e) {
+
+      burgerIcon.classList.toggle('active');
+      menuNav.classList.toggle('active');
+      document.body.classList.toggle('lock');
+
+      const logoHeader = document.querySelector('.header__logo');
+      logoHeader.classList.toggle('back');
+   })
+}
+
+//-----------------------navigation---------------------------------------------//
+
 const headerLinks = document.querySelectorAll('.header__nav-link[data-goto]');
 if (headerLinks.length > 0) {
    headerLinks.forEach(headerLink => {
@@ -11,6 +30,13 @@ if (headerLinks.length > 0) {
          const gotoBlock = document.querySelector(headerLink.dataset.goto);
          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
 
+         if (burgerIcon.classList.contains('active')) {
+            burgerIcon.classList.remove('active');
+            menuNav.classList.remove('active');
+            document.body.classList.remove('lock');
+
+         }
+
          window.scrollTo({
             top: gotoBlockValue,
             behavior: "smooth"
@@ -21,4 +47,4 @@ if (headerLinks.length > 0) {
 }
 
 let resultTask = '1. +10 \n2. +20 \n3. +48 \n4. +12 \n5. +20';
-console.log (resultTask);
+console.log(resultTask);
